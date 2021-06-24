777bd3f5eb5f717d1769d3dc8dcdd13dd1e60ff3
A-Albagshi <abdullah.albagshi@tuwaiq.edu.sa>
2021-06-24 03:27:56 +0300

    feat: setup react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useContext } from 'react';
 import './App.css';
+import AddPost from './Components/AddPost';
+import { AuthContext } from './Components/Auth/Auth';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Login from './Components/Auth/Login';
+import Posts from './Components/Posts';
+import Register from './Components/Auth/Register';
+import PostDetails from './Components/PostDetails';
+import UpdatePost from './Components/UpdatePost';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { currentUser } = useContext(AuthContext);
+
+	return (
+		<div className='App'>
+			<Switch>
+				<Route exact path='/' component={Posts} />
+				<Route exact path='/login' component={Login} />
+				<Route exact path='/register' component={Register} />
+				<Route exact path='/postdetails/:id' component={PostDetails} />
+				{currentUser != null ? (
+					<>
+						<Route exact path='/post/add' component={AddPost} />
+						<Route exact path='/postupdate/:id' component={UpdatePost} />
+					</>
+				) : (
+					<Redirect to={{ pathname: '/login' }} />
+				)}
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
